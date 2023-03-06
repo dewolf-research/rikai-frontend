82bbde09bc2745c29ab2db56e9ad9ca18e69d8b7
Niklas Bergmann <Niklas.Bergmann@fkie.fraunhofer.de>
2023-03-06 13:57:44 +0100

    removed blank line

--- rikai/pattern/__init__.py
+++ rikai/pattern/__init__.py
@@ -1,3 +1,2 @@
 """Module implementing behavior pattern and their components."""
 from .parser import Assignment, Behavior, Call, Literal, Operand, PatternParser, Rule, RuleParser, UnboundVariable, Variable
-
